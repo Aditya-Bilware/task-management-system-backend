8b93e34c7eb42798f9eb4e5af4932ab7e64cf9f7
Aditya Bilware <aditya.bilware@necsws.com>
2026-06-02 17:19:11 +0530

    cors  change

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://Aditya-Bilware.github.io"],
+    // origin: ["http://localhost:5173", "https://Aditya-Bilware.github.io"],
+    origin: true,
     credentials: true,
   }),
 );
