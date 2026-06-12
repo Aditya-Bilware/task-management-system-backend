e56b22416c832c3ca36f15dac027ff36002d446f
Aditya Bilware <aditya.bilware@necsws.com>
2026-06-12 15:04:48 +0530

    Removed normalized date helper

--- utils/normalizedUTCDate.js
+++
@@ -1,9 +0,0 @@
-const normalizeUTCDate = (date) => {
-  const d = new Date(date);
-
-  return new Date(
-    Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate()),
-  );
-};
-
-module.exports = { normalizeUTCDate };
