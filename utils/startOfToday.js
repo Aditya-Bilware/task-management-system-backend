a99851e220d0d02f319b81d17044e27848728f60
Aditya Bilware <aditya.bilware@necsws.com>
2026-06-24 11:28:02 +0530

    Trying to fix overdue issue

--- utils/startOfToday.js
+++ utils/startOfToday.js
@@ -11,7 +11,7 @@ const getStartOfTodayIST = () => {
 
   const now = new Date();
 
-  console.log("TMS-0067", new Date("2026-06-23T18:30:00.000Z") < today);
+  console.log("TMS-0067", new Date("2026-06-23T18:30:00.000Z") < now);
 
   const istDate = new Intl.DateTimeFormat("en-CA", {
     timeZone: "Asia/Kolkata",
