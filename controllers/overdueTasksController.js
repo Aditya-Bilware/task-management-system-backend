9f5759c1aa7ee674178a39c0a95a9b9ad758b4b4
Aditya Bilware <aditya.bilware@necsws.com>
2026-06-24 10:58:12 +0530

    Trying to fix overdue issue

--- controllers/overdueTasksController.js
+++ controllers/overdueTasksController.js
@@ -16,9 +16,23 @@ const getOverdueTasks = async (req, res) => {
 
     const today = getStartOfTodayIST();
 
-    console.log("TODAY", today.toISOString());
-    console.log("TODAY IST:", today);
+    console.log("TODAY:", today.toISOString());
 
+    const tasks = await Task.find({
+      status: {
+        $nin: ["done", "rejected"],
+      },
+    })
+      .select("taskNumber dueDate")
+      .lean();
+
+    tasks.forEach((task) => {
+      console.log(
+        task.taskNumber,
+        task.dueDate?.toISOString(),
+        task.dueDate < today,
+      );
+    });
     const filter = {
       isDeleted: false,
 
